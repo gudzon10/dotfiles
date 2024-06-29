3da08c055cbbade2d1681e7440bc48023d21ed42
Koltsov Kirill <kkoltsov@estp.ru>
2024-06-29 22:38:52 +0300

    install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
-BASEDIR=$(dirname $0)
-#-cat $(pwd)/.bash_logout >> $HOME/.bash_logout
-cat "$BASEDIR"/.bash_logout 
+SOURCE_DIR=$(dirname $0)
+
+if ! grep -q 'tac "$HISTFILE"' "$HOME"/.bash_logout; then
+    cat "$SOURCE_DIR"/.bash_logout >> $HOME/.bash_logout
+fi
+
+if ! grep -q '. ~/.bashconfrc' "$HOME"/.bashrc; then
+    cp "$SOURCE_DIR"/.bashconfrc  $HOME/
+    echo -e "\nif [ -f ~/.bashconfrc ]; then\n    . ~/.bashconfrc\nfi" >> "$BASHRC_FILE"
+fi
