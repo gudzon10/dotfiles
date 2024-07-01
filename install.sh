1ac4c302b40a22f29233192e409c43278aac4d94
Koltsov Kirill <kkoltsov@estp.ru>
2024-07-01 15:37:10 +0300

    blabla

--- install.sh
+++ install.sh
@@ -2,17 +2,15 @@
 
 SOURCE_DIR=$(dirname $0)
 
-if ! grep -q 'tac "$HISTFILE"' "$HOME"/.bash_logout; then
-    cat "$SOURCE_DIR"/.bash_logout >> $HOME/.bash_logout
-fi
+# if ! grep -q 'tac "$HISTFILE"' "$HOME"/.bash_logout; then
+#     cat "$SOURCE_DIR"/.bash_logout >> $HOME/.bash_logout
+# fi
+cp "$SOURCE_DIR"/.bash_logout $HOME/.bash_logout
+cp "$SOURCE_DIR"/.bashconfrc $HOME/.bashconfrc
 
 if ! grep -q '. ~/.bashconfrc' "$HOME"/.bashrc; then
     cp "$SOURCE_DIR"/.bashconfrc  $HOME/
     echo -e "\nif [ -f ~/.bashconfrc ]; then\n    . ~/.bashconfrc\nfi" >> "$HOME"/.bashrc
 fi
 
-
-source $HOME/.bashrc
-
-# Надо проверять установку всех скриптов, и инсталлироват их, если отсутствуют.
-# Надо вынести \h в переменную
+echo "Please, reload .bashrc manually:  source $HOME/.bashrc"
