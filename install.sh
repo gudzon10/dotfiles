b225a4849ef31f667a91358d86db9e902093d100
Koltsov Kirill <kkoltsov@estp.ru>
2024-07-01 15:53:02 +0300

    git prompt

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 SOURCE_DIR=$(dirname $0)
 
-# if ! grep -q 'tac "$HISTFILE"' "$HOME"/.bash_logout; then
-#     cat "$SOURCE_DIR"/.bash_logout >> $HOME/.bash_logout
-# fi
 cp "$SOURCE_DIR"/.bash_logout $HOME/.bash_logout
 cp "$SOURCE_DIR"/.bashconfrc $HOME/.bashconfrc
 
 if ! grep -q '. ~/.bashconfrc' "$HOME"/.bashrc; then
     cp "$SOURCE_DIR"/.bashconfrc  $HOME/
     echo -e "\nif [ -f ~/.bashconfrc ]; then\n    . ~/.bashconfrc\nfi" >> "$HOME"/.bashrc
+fi
+
+if [ ! -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 fi
 
 echo "Please, reload .bashrc manually:  source $HOME/.bashrc"
