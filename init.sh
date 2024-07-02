e66135259ce65cc0d8acff5abe646b9204ea43c0
Koltsov Kirill <kkoltsov@estp.ru>
2024-07-02 16:57:04 +0300

    blabla

--- init.sh
+++ init.sh
@@ -1,1 +1,28 @@
 #!/usr/bin/bash
+
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && flatpak update -y
+
+
+sudo apt install -y \
+    git vim xclip \
+    ca-certificates curl \
+
+
+### Установка Docker
+
+## Удаление старых версий
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt -y remove $pkg; done
+
+## Добавление репозитория
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+## Обновление и установка
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
