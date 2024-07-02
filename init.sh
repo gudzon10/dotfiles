e36e88f3e040bcc0aa1c120ba40c364ce806cd3b
Koltsov Kirill <kkoltsov@estp.ru>
2024-07-02 17:04:39 +0300

    bla

--- init.sh
+++ init.sh
@@ -26,3 +26,7 @@ echo \
 
 sudo apt update
 sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
