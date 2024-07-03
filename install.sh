29cfe15842c0102e2d9e1958100ecc7fa790eba6
Koltsov Kirill <kkoltsov@estp.ru>
2024-07-03 13:55:39 +0300

    bla

--- install.sh
+++ install.sh
@@ -14,4 +14,22 @@ if [ ! -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 fi
 
+## Обновления по крону.
+#!/bin/bash
+
+if [ ! -f /var/log/system-update.log ]; then
+    touch /var/log/system-update.log
+    chown "$USER":"$USER" /var/log/system-update.log
+fi
+sudo cp "$SOURCE_DIR"/fullupgrade /usr/local/bin/fullupgrade
+
+# Получаем содержимое задачи
+CRON_TASK="0 * * * * /usr/local/bin/fullupgrade"
+# Проверяем, есть ли уже задача в cron-таблице
+if ! crontab -l | grep -q "/usr/local/bin/fullupgrade"; then
+  # Если нет, добавляем задачу
+  (crontab -l; echo "$CRON_TASK") | crontab -
+fi
+
+
 echo "Please, reload .bashrc manually:  source $HOME/.bashrc"
